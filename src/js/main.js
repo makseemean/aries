509149ddf4159115e5358b715d3d7bfbbe1d2981
Maksim <maksim.podolinniy@gmail.com>
2024-01-08 14:59:08 +0300

    final edits

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,55 @@
 // Scripts for Program page
 
+// Scroll bar width
+
+const div = document.createElement('div');
+
+div.style.cssText = 'overflow-y: scroll; height: 50px; width: 50px;';
+
+document.body.append(div);
+
+const scrollWidth = div.offsetWidth - div.clientWidth;
+
+console.log(scrollWidth)
+
+div.remove();
+
+// Aside menu
+
+function openMenu() {
+   asideMenu.classList.add('aside-menu_active');
+   overlay.classList.add('overlay_active');
+   logo.classList.add('logo_hide');
+   document.body.style.cssText = `overflow: hidden; padding-right: ${scrollWidth}px`;
+};
+
+function closeMenu() {
+   asideMenu.classList.remove('aside-menu_active');
+   overlay.classList.remove('overlay_active');
+   logo.classList.remove('logo_hide');
+   document.body.style.cssText = 'overflow: auto; padding-right: 0`';
+};
+
+const menuBtn = document.querySelector('.aside-menu-btn'),
+   menuClose = document.querySelector('.aside-menu__close'),
+   asideMenu = document.querySelector('.aside-menu'),
+   overlay = document.querySelector('.overlay'),
+   logo = document.querySelector('.logo');
+
+menuBtn.addEventListener('click', () => {
+   openMenu();
+});
+
+menuClose.addEventListener('click', () => {
+   closeMenu();
+});
+
+overlay.addEventListener('click', (e) => {
+   if (!e.target.classList.contains('aside-menu')) {
+      closeMenu();
+   }
+});
+
 if (document.querySelector('.program-page')) {
 
    // Click for details btn
